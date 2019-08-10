b11cec999793975f3d9023ad4873f0833adf5ddb
Kevin Luedemann <kevin27luedemann@googlemail.com>
2019-08-10 16:38:34 +0200

    Better handling and continues output

--- obd2/car_stuff.py
+++ obd2/car_stuff.py
@@ -5,15 +5,7 @@ import time
 from pprint import pprint
 import json
 import signal
-
-cmds = ["SPEED",
-        "RPM",
-        "ENGINE_LOAD",
-        "THROTTLE_POS",
-        "FUEL_LEVEL",
-        "COOLANT_TEMP",
-        "DISTANCE_W_MIL"
-        ]
+from optparse import OptionParser
 
 interupt_flag   = True
 def signal_handler(sig, frame):
@@ -23,28 +15,58 @@ signal.signal(signal.SIGINT, signal_handler)
     
 def main():
     global interupt_flag
-    save    = True
-    con     = obd.OBD("/dev/serial0",115200)
-    print(con.status())
+    parser = OptionParser()
+    #parser.add_option("-f", "--file", dest="filename",
+    #                  help="write report to FILE", metavar="FILE")
+    parser.add_option("-s", "--save",
+                      action="store_true", dest="save", default=False,
+                      help="Save data to file")
+    parser.add_option("-q", "--quiet",
+                      action="store_true", dest="quiet", default=False,
+                      help="don't print anything")
+    parser.add_option("", "--debug",
+                      action="store_true", dest="debug", default=False,
+                      help="print debug output")
+
+    (options, args) = parser.parse_args()
+
+    con     = obd.OBD("/dev/serial0",115200,fast=False)
+    if not(options.quiet):
+        print(con.status())
+    cmdsup  = con.supported_commands
+    if options.debug:
+        obd.logger.setLevel(obd.logging.DEBUG)
+    cmds    = []
+    for cmd in cmdsup:
+        if  not("DTC" in cmd.name) and \
+            not("MONITOR" in cmd.name) and \
+            not("PID" in cmd.name) and \
+            not("VERSION" in cmd.name) and \
+            not("OBD" in cmd.name) and \
+            not("STATUS" in cmd.name) and \
+            not("MID" in cmd.name) and \
+            not("O2_SENSORS" in cmd.name):
+            cmds.append(cmd.name)
     counter = 0
     data    = {}
     while(interupt_flag):
         status = {}
         for cmd in sorted(cmds):
-            status.update({cmd:str(con.query(obd.commands[cmd],force=True).value)})
+            status.update({cmd:str(con.query(obd.commands[cmd]).value)})
         now   = dt.datetime.strftime(dt.datetime.now(),"%Y%m%d_%H%M%S")
         data.update({now:status})
-        pprint(status)
+        if not(options.quiet):
+            pprint(status)
         time.sleep(2)
         counter = counter + 1
         if counter >= 600:
-            if save:
-                with open(now+".dat","w+") as dafi:
+            if options.save:
+                with open(now+".dat","a") as dafi:
                     json.dump(data,dafi,sort_keys=True,indent=4)
             data    = {}
             counter = 0
 
-    if save:
+    if options.save:
         now   = dt.datetime.strftime(dt.datetime.now(),"%Y%m%d_%H%M%S")
         with open(now+".dat","w+") as dafi:
             json.dump(data,dafi,sort_keys=True,indent=4)
