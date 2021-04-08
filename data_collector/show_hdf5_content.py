1f068c573bbaead87875fe7518dff6270803d2a5
Kevin Luedemann <kevin27luedemann@googlemail.com>
2021-04-08 17:27:50 +0200

    PLot for showing content is written

--- data_collector/show_hdf5_content.py
+++ data_collector/show_hdf5_content.py
@@ -6,22 +6,25 @@ import animation as an
 import datetime as dt
 
 def ti_to_dt(ti):
-    return dt.datetime(2000,1,1)+ti
-    
+    return dt.datetime(2000,1,1)+dt.timedelta(seconds=ti)
 
-def animate_timelaps(f):
+def ti_to_dt_str(ti):
+    dti     = dt.datetime(2000,1,1)+dt.timedelta(seconds=ti)
+    return dt.datetime.strftime(dti,"%H:%M:%S %d.%m.%Y")
+
+def animate_timelaps(f,fps=2):
     d_pic       = f["/timelaps"]
     d_pti       = f["/timelaps_ti"]
-    print(d_pic.shape)
-    print(d_pti.shape)
-    it,dy,dx    = dset.shape
+    it,dy,dx    = d_pic.shape
 
-    ani = an.anim.anim( [dset],
-                        ["{}".format(ti_to_dt(ti)) for ti in d_pti[:]],
+    title       = [ti_to_dt_str(ti[0]) for ti in d_pti[:]]
+
+    ani = an.anim.anim( [d_pic],
+                        title,
                         num_axis=1,
                         max_count=it)
-    ani.set_FPS(10)
-    im          = ani.ax.imshow(dset[0,:,:],cmap="gray")
+    ani.set_FPS(fps)
+    im          = ani.ax.imshow(d_pic[0,:,:],cmap="gray")
     ani.ax.axis(False)
 
     def update(ax,li,data,title,num):
@@ -32,7 +35,38 @@ def animate_timelaps(f):
     ani.animate([im],update)
     plt.show()
 
+def show_gps_time_data(f):
+    d_GPS   = f["/GPS"]
+    print(d_GPS[0])
+    dtis    = [ti_to_dt(igps[0]) for igps in d_GPS[:,:]]
+    dtis    = np.array(dtis)
+
+    fig,ax  = plt.subplots(3,sharex=True)
+
+    #Ther is something wrong with the erorr form GPSD
+    #ax[0].errorbar(dtis,d_GPS[:,1],yerr=d_GPS[:,8])
+    #ax[1].errorbar(dtis,d_GPS[:,2],yerr=d_GPS[:,9])
+    #ax[2].errorbar(dtis,d_GPS[:,3],yerr=d_GPS[:,10])
+    ax[0].plot(dtis,d_GPS[:,1],"r+-")
+    ax[1].plot(dtis,d_GPS[:,2],"r+-")
+    ax[2].plot(dtis,d_GPS[:,3],"r+-")
+    
+    ax[0].set_ylabel("Latitude")
+    ax[1].set_ylabel("Longitude")
+    ax[2].set_ylabel("Speed/ m s^-1")
+
+    fig.autofmt_xdate()
+
+    #Plot horizontal position
+    fig2,ax2= plt.subplots()
+    ax2.plot(d_GPS[:,1],d_GPS[:,2],"r+-")
+    ax2.set_xlabel("Latitude")
+    ax2.set_ylabel("Longitude")
+
+    plt.show()
+
 if __name__ == "__main__":
-    f           = h5py.File("./demo.hdf5","r",swmr=True)
-    animate_timelaps()
+    f           = h5py.File("./20210408.hdf5","r",swmr=True)
+    #animate_timelaps(f)
+    show_gps_time_data(f)
     f.close()
