01fc3e5212c3a7d4d94ec5d09b02f843c5c6e6a2
Kevin Luedemann <kevin27luedemann@googlemail.com>
2021-08-11 20:54:43 +0200

    Clean up cam file

--- data_collector/cam.py
+++ data_collector/cam.py
@@ -72,12 +72,6 @@ class MotionDetec(array.PiMotionAnalysis):
             motion_detected         = False
             self.no_motion_frames   = 0
 
-def init_gps():
-    # Connect to the local gpsd
-    gpsd.connect()
-    #dummy read which is needed at the beginning for some reason
-    collect_gps_data()
-
 def init_camera(reso=(3280,2464),framerate=30,loglevel=2):
     camera = PiCamera()
     if camera.revision == "imx219":
@@ -109,51 +103,8 @@ def take_picture(camera):
     #camera.capture(output, format='yuv', use_video_port=True,resize=(1920,1080))
     return output.array[:,:,0]
 
-def collect_gps_data():
-    packet = gpsd.get_current()
-
-    #initialize all values with NAN for better filtering
-    data    = np.ones(6)*np.nan
-    error   = np.ones(6)*np.nan
-
-    #Set the values that are available depending on the mode
-    if packet.mode >= 2:
-        data[0] = packet.lat
-        data[1] = packet.lon
-        data[2] = packet.hspeed
-        data[3] = packet.track
-        error[0]= packet.error["t"]
-        error[1]= packet.error["y"]
-        error[2]= packet.error["x"]
-        error[3]= packet.error["s"]
-    if packet.mode >= 3:
-        data[4] = packet.alt
-        data[5] = packet.climb
-        error[4]= packet.error["c"]
-        error[5]= packet.error["v"]
-
-    return packet.time,data,error
-
-def time_diff_sec(dti):
-    return (dti-dt.datetime(2000,1,1)).total_seconds()
-
-def format_gps_data(blocking=False):
-    ti,da,er    = collect_gps_data()
-    if ti == '' and blocking:
-        while ti == '':
-            time.sleep(0.01)
-            ti,da,er    = collect_gps_data()
-        dti     = dt.datetime.strptime(ti,"%Y-%m-%dT%H:%M:%S.%fZ")
-    else:
-        dti     = dt.datetime.now()
-    ti_sec      = time_diff_sec(dti)
-    GPS_point   = np.append(da,er,axis=0)
-    GPS_point   = np.append(np.array([ti_sec]),GPS_point,axis=0)
-    return GPS_point
-
 def loop(   camera,
             fname_data,
-            sensor,
             praefix="",
             loglevel=1,
             concat=False,
@@ -164,11 +115,7 @@ def loop(   camera,
     global keep_running
 
     #setting up GPS buffer
-    l_data_ti       = 0.
-    GPS_point       = format_gps_data()
     speed           = np.ones(30)
-    counter         = 0
-    fi_data         = open(fname_data,"a")
 
     #Use circular io buffor
     if loglevel == 0:
@@ -208,44 +155,7 @@ def loop(   camera,
             stream.clear()
             while motion_detected:
                 camera.wait_recording(0.5)
-                #Handle GPS and sensor data once every second
-                c_ti    = time.time()
-                if c_ti - l_data_ti >= 1.:
-                    if loglevel == 0:
-                        print("counter={}".format(counter))
-                    GPS_point       = format_gps_data()
-                    #Speed in km/h
-                    speed[counter]  = GPS_point[3]*3.6
-                    spee        = np.mean(speed)
-                    if loglevel == 0:
-                        print("speed={}".format(spee))
 
-                    if spee <= 5.0:
-                        mclass.threshold              = 25
-                        mclass.num_blocks             = 3
-                    else:
-                        mclass.threshold              = 80
-                        mclass.num_blocks             = 7
-
-                    out = ""
-                    for da in GPS_point:
-                        out += "{}\t".format(da)
-
-                    sensor.icm20948_Gyro_Accel_Read()
-                    gs      = np.copy(sense.Accel)/16384
-
-                    for g in gs:
-                        out += "{}\t".format(g)
-
-                    out += "\n"
-                    if loglevel == 0:
-                        print(out)
-                    fi_data.write(out)
-                    fi_data.flush()
-
-                    l_data_ti   = c_ti
-                    counter    += 1
-                    counter    %= 30
             if loglevel == 0:
                 print("Motion done, splitting back to circular io")
             camera.split_recording(stream,splitter_port=1)
@@ -266,47 +176,6 @@ def loop(   camera,
                     print("Running ffmpeg command")
                 os.system(command)
 
-        #Handle GPS data once every second
-        c_ti    = time.time()
-        if c_ti - l_data_ti >= 1.:
-            if loglevel == 0:
-                print("counter={}".format(counter))
-            GPS_point       = format_gps_data()
-            #Speed in km/h
-            speed[counter]  = GPS_point[3]*3.6
-            spee        = np.mean(speed)
-            if loglevel == 0:
-                print("speed={}".format(spee))
-
-            if spee <= 5.0:
-                mclass.threshold              = 25
-                mclass.num_blocks             = 3
-            else:
-                mclass.threshold              = 80
-                mclass.num_blocks             = 7
-
-            out = ""
-            for da in GPS_point:
-                out += "{}\t".format(da)
-
-            sensor.icm20948_Gyro_Accel_Read()
-            gs      = np.copy(sense.Accel)/16384
-
-            for g in gs:
-                out += "{}\t".format(g)
-
-            out += "\n"
-
-            if loglevel == 0:
-                print(out)
-            fi_data.write(out)
-            fi_data.flush()
-
-            l_data_ti   = c_ti
-            counter    += 1
-            counter    %= 30
-
-    fi_data.close()
     camera.stop_recording(splitter_port=2)
     camera.stop_recording()
 
@@ -358,8 +227,6 @@ if __name__ == "__main__":
     if options.loglevel == 0:
         print(fname)
 
-    sensor              = sense.ICM20948()
-    init_gps()
     cam                 = init_camera(loglevel=int(options.loglevel))
 
     if options.create_mask:
@@ -377,7 +244,6 @@ if __name__ == "__main__":
 
         loop(   cam,
                 fname,
-                sensor,
                 praefix=options.praefix,
                 loglevel=int(options.loglevel),
                 concat=options.concat,
